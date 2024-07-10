6bb5679aae59e5549926178b103bdf17dd05a6d2
Bench Instance for buffsrv2 <frappe@buffsrv2.buff0k.co.za>
2024-07-10 13:00:22 +0200

    First commit

---
+++ ir/industrial_relations/doctype/offence_outcome/offence_outcome.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, BuFf0k and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OffenceOutcome(Document):
+	pass
