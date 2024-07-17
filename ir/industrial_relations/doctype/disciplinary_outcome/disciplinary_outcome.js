f2fa2fcd5de39a9f2ed6c5a41c005d587333f82d
buff0k <buff0k@gmail.com>
2024-07-17 14:03:25 +0000

    new file:   ir/industrial_relations/doctype/disciplinary_outcome/__init__.py 	new file:   ir/industrial_relations/doctype/disciplinary_outcome/disciplinary_outcome.js 	new file:   ir/industrial_relations/doctype/disciplinary_outcome/disciplinary_outcome.json 	new file:   ir/industrial_relations/doctype/disciplinary_outcome/disciplinary_outcome.py 	new file:   ir/industrial_relations/doctype/disciplinary_outcome/test_disciplinary_outcome.py 	modified:   ir/industrial_relations/doctype/warning_form/warning_form.json 	new file:   ir/industrial_relations/module_onboarding/industrial_relations/industrial_relations.json 	new file:   ir/industrial_relations/onboarding_step/company_set_up/company_set_up.json 	new file:   ir/industrial_relations/onboarding_step/create_disciplinary_offence_category/create_disciplinary_offence_category.json 	new file:   ir/industrial_relations/onboarding_step/create_employee/create_employee.json 	new file:   ir/industrial_relations/onboarding_step/create_outcome/create_outcome.json 	new file:   ir/industrial_relations/onboarding_step/create_specific_desciplinary_offence/create_specific_desciplinary_offence.json 	modified:   ir/industrial_relations/workspace/industrial_relations/industrial_relations.json Add Onboarding to Dashboard

---
+++ ir/industrial_relations/doctype/disciplinary_outcome/disciplinary_outcome.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, BuFf0k and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Disciplinary Outcome", {
+// 	refresh(frm) {
+
+// 	},
+// });
