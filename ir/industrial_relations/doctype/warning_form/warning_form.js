240531ca0c30e02ff021806bef37be9f5f360a0a
buff0k <buff0k@gmail.com>
2024-07-18 11:10:21 +0000

    modified:   ir/industrial_relations/doctype/nta_hearing/nta_hearing.js 	modified:   ir/industrial_relations/doctype/warning_form/warning_form.js Fixed issue where nta_hearing and warning_form client scripts aren't triggered when forms are generated from disiciplinary_action buttons. optimized functions as this seems to be final client code at this time.

--- ir/industrial_relations/doctype/warning_form/warning_form.js
+++ ir/industrial_relations/doctype/warning_form/warning_form.js
@@ -2,101 +2,74 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Warning Form', {
-    linked_disciplinary_action: function(frm, cdt, cdn) {
-        //Fetch accused field from Disciplinary Action document and enter into name field
+    refresh: function(frm) {
+        // Manually trigger the linked_disciplinary_action handler if the field is already set
         if (frm.doc.linked_disciplinary_action) {
-            frappe.db.get_value('Disciplinary Action', frm.doc.linked_disciplinary_action, 'accused', (a) => {
-                if (a && a.accused) {
-                    frm.set_value('employee', a.accused);
-                } else {
-                    frm.set_value('employee', '');
-                }
-            });
+            frm.trigger('linked_disciplinary_action');
         }
-        //Fetch data from the previous_disciplinary_outcomes field in the Disciplinary Action document and enter into the disciplinary_history field
+    },
+    
+    linked_disciplinary_action: function(frm) {
         if (frm.doc.linked_disciplinary_action) {
-            frappe.model.with_doc("Disciplinary Action", frm.doc.linked_disciplinary_action, function() {
-                var b = frappe.model.get_doc("Disciplinary Action", frm.doc.linked_disciplinary_action);
-                cur_frm.clear_table("disciplinary_history");
-                $.each(b.previous_disciplinary_outcomes, function(c, d) {
-                    c = frm.add_child("disciplinary_history");
-                    c.disc_action = d.disc_action;
-                    c.date = d.date;
-                    c.sanction = d.sanction;
-                    c.charges = d.charges;
-                });
-                cur_frm.refresh_field("disciplinary_history");
+            // Fetch accused field from Disciplinary Action document and enter into employee field
+            frappe.db.get_value('Disciplinary Action', frm.doc.linked_disciplinary_action, 'accused', (r) => {
+                frm.set_value('employee', r ? r.accused : '');
             });
-        }
-        //Fetch data from the final_charges field in the Disciplinary Action document and enter into the warning_charges field
-        if (frm.doc.linked_disciplinary_action) {
-            frappe.model.with_doc("Disciplinary Action", frm.doc.linked_disciplinary_action, function() {
-                var e = frappe.model.get_doc("Disciplinary Action", frm.doc.linked_disciplinary_action);
-                cur_frm.clear_table("warning_charges");
-                $.each(e.final_charges, function(_, f) {
-                    var g = frm.add_child("warning_charges");
-                    g.warning_charge = `(${f.code_item}) ${f.charge}`;
-                    
-                });
-                cur_frm.refresh_field("warning_charges");
-            });
-}
 
-}});
-frappe.ui.form.on('Warning Form', {
+            // Fetch data from previous_disciplinary_outcomes and enter into disciplinary_history field
+            frappe.model.with_doc('Disciplinary Action', frm.doc.linked_disciplinary_action, function() {
+                let doc = frappe.get_doc('Disciplinary Action', frm.doc.linked_disciplinary_action);
+                frm.clear_table('disciplinary_history');
+                $.each(doc.previous_disciplinary_outcomes, function(_, row) {
+                    let child = frm.add_child('disciplinary_history');
+                    child.disc_action = row.disc_action;
+                    child.date = row.date;
+                    child.sanction = row.sanction;
+                    child.charges = row.charges;
+                });
+                frm.refresh_field('disciplinary_history');
+
+                // Fetch data from final_charges and enter into warning_charges field
+                frm.clear_table('warning_charges');
+                $.each(doc.final_charges, function(_, row) {
+                    let child = frm.add_child('warning_charges');
+                    child.warning_charge = `(${row.code_item}) ${row.charge}`;
+                });
+                frm.refresh_field('warning_charges');
+            });
+        }
+    },
+
     employee: function(frm) {
-        //Fetch employee_name field from Employee document and enter into names field
         if (frm.doc.employee) {
-            frappe.db.get_value('Employee', frm.doc.employee, 'employee_name', (h) => {
-                if (h && h.employee_name) {
-                    frm.set_value('names', h.employee_name);
-                } else {
-                    frm.set_value('names', '');
-                }
+            // Fetch employee_name field from Employee document and enter into names field
+            frappe.db.get_value('Employee', frm.doc.employee, 'employee_name', (r) => {
+                frm.set_value('names', r ? r.employee_name : '');
+            });
+
+            // Fetch employee field from Employee document and enter into coy field
+            frappe.db.get_value('Employee', frm.doc.employee, 'employee', (r) => {
+                frm.set_value('coy', r ? r.employee : '');
+            });
+
+            // Fetch designation field from Employee document and enter into position field
+            frappe.db.get_value('Employee', frm.doc.employee, 'designation', (r) => {
+                frm.set_value('position', r ? r.designation : '');
+            });
+
+            // Fetch company field from Employee document and enter into company field
+            frappe.db.get_value('Employee', frm.doc.employee, 'company', (r) => {
+                frm.set_value('company', r ? r.company : '');
             });
         }
-        //Fetch employee field from Employee document and enter into coy field
-        if (frm.doc.employee) {
-            frappe.db.get_value('Employee', frm.doc.employee, 'employee', (i) => {
-                if (i && i.employee) {
-                    frm.set_value('coy', i.employee);
-                } else {
-                    frm.set_value('coy', '');
-                }
-            });
-        }
-        //Fetch designation field from Employee document and enter into position field
-        if (frm.doc.employee) {
-            frappe.db.get_value('Employee', frm.doc.employee, 'designation', (j) => {
-                if (j && j.designation) {
-                    frm.set_value('position', j.designation);
-                } else {
-                    frm.set_value('position', '');
-                }
-            });
-        }
-        //Fetch company field from the Employee document and enter into company field
-        if (frm.doc.employee) {
-            frappe.db.get_value('Employee', frm.doc.employee, 'company', (k) => {
-                if (k && k.company) {
-                    frm.set_value('company', k.company);
-                } else {
-                    frm.set_value('company', '');
-                }
+    },
+
+    company: function(frm) {
+        if (frm.doc.company) {
+            // Fetch default_letter_head field from Company document and enter into letter_head field
+            frappe.db.get_value('Company', frm.doc.company, 'default_letter_head', (r) => {
+                frm.set_value('letter_head', r ? r.default_letter_head : '');
             });
         }
     }
 });
-frappe.ui.form.on('Warning Form', {
-    company: function(frm) {
-        //Fetch default_letter_head field from the Company document and enter into the letter_head field
-        if (frm.doc.company) {
-            frappe.db.get_value('Company', frm.doc.company, 'default_letter_head', (l) => {
-               if (l && l.default_letter_head) {
-                    frm.set_value('letter_head', l.default_letter_head);
-                } else {
-                    frm.set_value('letter_head', '');
-                }
-            });
-        }
-}});
