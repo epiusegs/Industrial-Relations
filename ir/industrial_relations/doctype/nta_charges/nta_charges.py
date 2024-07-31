760fb95ffa3a1fbaa3db20153f397a360163cbf5
buff0k <buff0k@gmail.com>
2024-07-31 07:12:10 +0000

    renamed:    ir/industrial_relations/doctype/nta_charges/__init__.py -> ir/industrial_relations/doctype/charges/__init__.py 	renamed:    ir/industrial_relations/doctype/nta_charges/nta_charges.json -> ir/industrial_relations/doctype/charges/charges.json 	renamed:    ir/industrial_relations/doctype/nta_charges/nta_charges.py -> ir/industrial_relations/doctype/charges/charges.py 	modified:   ir/industrial_relations/doctype/demotion_form/demotion_form.json 	modified:   ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.json 	modified:   ir/industrial_relations/doctype/dismissal_form/dismissal_form.json 	modified:   ir/industrial_relations/doctype/not_guilty_form/not_guilty_form.json 	modified:   ir/industrial_relations/doctype/nta_hearing/nta_hearing.json 	modified:   ir/industrial_relations/doctype/pay_deduction_form/pay_deduction_form.json 	modified:   ir/industrial_relations/doctype/suspension_form/suspension_form.json 	deleted:    ir/industrial_relations/doctype/warning_charges/__init__.py 	deleted:    ir/industrial_relations/doctype/warning_charges/warning_charges.json 	deleted:    ir/industrial_relations/doctype/warning_charges/warning_charges.py 	modified:   ir/industrial_relations/doctype/warning_form/warning_form.json Combined redundant child tables and configured document numbering

--- ir/industrial_relations/doctype/nta_charges/nta_charges.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, BuFf0k and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class NTACharges(Document):
-	pass
