2e1d50990032edf8a4df0e7d2864e04ebfc719d5
buff0k <buff0k@gmail.com>
2024-08-06 12:07:43 +0000

    modified:   ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.js 	modified:   ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.json 	modified:   ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.py Updated Outcoem Report DocType

--- ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.js
+++ ir/industrial_relations/doctype/disciplinary_outcome_report/disciplinary_outcome_report.js
@@ -1,8 +1,79 @@
 // Copyright (c) 2024, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Disciplinary Outcome Report", {
-// 	refresh(frm) {
+frappe.ui.form.on("Disciplinary Outcome Report", {
+    refresh: function(frm) {
+        // Check the flag before triggering the handler
+        if (frm.doc.linked_disciplinary_action && !frm.doc.linked_disciplinary_action_processed) {
+            frm.trigger('linked_disciplinary_action');
+        }
+    },
+    
+    linked_disciplinary_action: function(frm) {
+        if (frm.doc.linked_disciplinary_action) {
+            frappe.call({
+                method: 'ir.industrial_relations.doctype.disciplinary_outcome_report.disciplinary_outcome_report.fetch_disciplinary_action_data',
+                args: {
+                    disciplinary_action: frm.doc.linked_disciplinary_action
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        const data = r.message;
 
-// 	},
-// });
+                        // Directly update the doc and refresh fields without triggering events
+                        frm.doc.employee = data.accused || '';
+                        frm.doc.names = data.accused_name || '';
+                        frm.doc.coy = data.accused_coy || '';
+                        frm.doc.position = data.accused_pos || '';
+                        frm.doc.company = data.company || '';
+                        frm.doc.linked_nta = data.linked_nta || '';
+
+                        frm.refresh_field('employee');
+                        frm.refresh_field('names');
+                        frm.refresh_field('coy');
+                        frm.refresh_field('position');
+                        frm.refresh_field('company');
+                        frm.refresh_field('linked_nta');
+
+                        // Update child tables
+                        frm.clear_table('disciplinary_history');
+                        $.each(data.previous_disciplinary_outcomes, function(_, row) {
+                            let child = frm.add_child('disciplinary_history');
+                            child.disc_action = row.disc_action;
+                            child.date = row.date;
+                            child.sanction = row.sanction;
+                            child.charges = row.charges;
+                        });
+                        frm.refresh_field('disciplinary_history');
+
+                        frm.clear_table('outcome_charges');
+                        $.each(data.final_charges, function(_, row) {
+                            let child = frm.add_child('outcome_charges');
+                            child.indiv_charge = row.indiv_charge;
+                        });
+                        frm.refresh_field('outcome_charges');
+                        // Set the flag to prevent refresh loop
+                        frm.set_value('linked_disciplinary_action_processed', true);
+                    }
+                }
+            });
+        }
+    },
+
+    company: function(frm) {
+        if (frm.doc.company) {
+            frappe.call({
+                method: 'ir.industrial_relations.doctype.disciplinary_outcome_report.disciplinary_outcome_report.fetch_company_letter_head',
+                args: {
+                    company: frm.doc.company
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.doc.letter_head = r.message.letter_head || '';
+                        frm.refresh_field('letter_head');
+                    }
+                }
+            });
+        }
+    }
+});
