bcb19fa48f015d854e577909ca9b9657650d5cb5
buff0k <buff0k@gmail.com>
2024-08-21 16:46:21 +0000

    modified:   ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.js 	modified:   ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.json 	modified:   ir/industrial_relations/workspace/industrial_relations/industrial_relations.json Updated Workspace for new DocTypes, modified Contract of Employment DocType and Logic for testing fetch of data.

--- ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.js
+++ ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.js
@@ -4,39 +4,21 @@
 frappe.ui.form.on('Contract of Employment', {
     employee: function(frm) {
         if (frm.doc.employee) {
-            frappe.call({
-                method: 'frappe.client.get',
-                args: {
-                    doctype: 'Employee',
-                    name: frm.doc.employee
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value('employee_name', r.message.employee_name);
-                        frm.set_value('date_of_birth', r.message.date_of_birth);
-                        frm.set_value('date_of_joining', r.message.date_of_joining);
-                        frm.set_value('designation', r.message.designation);
-                        frm.set_value('current_address', r.message.current_address);
-                        frm.set_value('custom_id_number', r.message.custom_id_number);
-                        frm.set_value('branch', r.message.branch);
-                        
-                        // Fetch company details
-                        if (r.message.company) {
-                            frappe.call({
-                                method: 'frappe.client.get',
-                                args: {
-                                    doctype: 'Company',
-                                    name: r.message.company
-                                },
-                                callback: function(r2) {
-                                    if (r2.message) {
-                                        frm.set_value('company', r.message.company);
-                                        frm.set_value('letter_head', r2.message.default_letter_head);
-                                    }
-                                }
-                            });
-                        }
-                    }
+            frappe.db.get_doc('Employee', frm.doc.employee).then(doc => {
+                frm.set_value('employee_name', doc.employee_name || '');
+                frm.set_value('date_of_birth', doc.date_of_birth || '');
+                frm.set_value('date_of_joining', doc.date_of_joining || '');
+                frm.set_value('company', doc.company || '');
+                frm.set_value('designation', doc.designation || '');
+                frm.set_value('current_address', doc.current_address || '');
+                frm.set_value('custom_id_number', doc.custom_id_number || '');
+                frm.set_value('branch', doc.branch || '');
+
+                // Fetch default letter head
+                if (doc.company) {
+                    frappe.db.get_value('Company', doc.company, 'default_letter_head', (r) => {
+                        frm.set_value('letter_head', r.default_letter_head || '');
+                    });
                 }
             });
         }
@@ -44,44 +26,52 @@ frappe.ui.form.on('Contract of Employment', {
 
     contract_type: function(frm) {
         if (frm.doc.contract_type) {
-            frappe.call({
-                method: 'frappe.client.get',
-                args: {
-                    doctype: 'Contract Type',
-                    name: frm.doc.contract_type
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value('has_expiry', r.message.has_expiry);
-                        frm.set_value('has_retirement', r.message.has_retirement);
-                        frm.set_value('retirement_age', r.message.retirement_age);
-                        
-                        // Clear clauses table
-                        frm.clear_table('contract_clauses');
-                        
-                        // Populate clauses table
-                        r.message.sections.forEach(section => {
-                            let new_row = frm.add_child('contract_clauses');
-                            new_row.section_number = section.sec_no;
-                            frappe.call({
-                                method: 'frappe.client.get',
-                                args: {
-                                    doctype: 'Contract Section',
-                                    name: section.section
-                                },
-                                callback: function(sec) {
-                                    if (sec.message) {
-                                        new_row.clause_content = sec.message.sec_par.map(par => {
-                                            return `<p>${par.par_num}. ${par.par_text}</p>`;
-                                        }).join('');
-                                    }
-                                    frm.refresh_field('contract_clauses');
-                                }
-                            });
+            frappe.model.with_doc('Contract Type', frm.doc.contract_type, function() {
+                let contract_type_doc = frappe.get_doc('Contract Type', frm.doc.contract_type);
+
+                // Update fields in Contract of Employment
+                frm.set_value('has_expiry', contract_type_doc.has_expiry || 0);
+                frm.set_value('has_retirement', contract_type_doc.has_retirement || 0);
+                frm.set_value('retirement_age', contract_type_doc.retirement_age || '');
+
+                // Clear contract_clauses table
+                frm.clear_table('contract_clauses');
+
+                // Populate contract_clauses table based on contract_terms in Contract Type
+                contract_type_doc.contract_terms.forEach(term_row => {
+                    frappe.model.with_doc('Contract Section', term_row.section, function() {
+                        let section_doc = frappe.get_doc('Contract Section', term_row.section);
+
+                        let clause_content = `<b>${section_doc.sec_head}</b><br>`;
+                        section_doc.sec_par.forEach(paragraph => {
+                            clause_content += `${paragraph.par_num}. ${paragraph.par_text}<br>`;
                         });
-                    }
+
+                        let row = frm.add_child('contract_clauses');
+                        row.section_number = term_row.sec_no;
+                        row.clause_content = clause_content;
+
+                        frm.refresh_field('contract_clauses');
+                    });
+                });
+
+                // Handle has_retirement logic
+                if (contract_type_doc.has_retirement) {
+                    let retirement_age = contract_type_doc.retirement_age;
+                    let notification_date = frappe.datetime.add_months(frappe.datetime.add_years(frm.doc.date_of_birth, retirement_age), -1);
+                    // Add notification logic here if needed
+                }
+
+                // Handle has_expiry logic
+                if (contract_type_doc.has_expiry) {
+                    frm.set_df_property('end_date', 'reqd', 1);
+                    frm.set_df_property('project', 'reqd', 1);
+                } else {
+                    frm.set_df_property('end_date', 'reqd', 0);
+                    frm.set_df_property('project', 'reqd', 0);
                 }
             });
         }
     }
 });
+
