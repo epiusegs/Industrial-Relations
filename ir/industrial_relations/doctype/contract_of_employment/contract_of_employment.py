2b0cd08b46e2a3c80711c3915c1ef6324c73b626
buff0k <buff0k@gmail.com>
2024-08-22 08:35:16 +0000

    modified:   ir/fixtures/custom_docperm.json 	modified:   ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.py 	modified:   ir/industrial_relations/doctype/contract_paragraph/contract_paragraph.json 	modified:   ir/industrial_relations/doctype/contract_type/contract_type.json Most of the heavy lifting in the Contract of Employment DocType is completed, needs some quality of life adjustments (intendation of paragraphs) and Print Design to be finalized, also applying a link filter to the contract type to only permit Submitted "Live" Contract Types

--- ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.py
+++ ir/industrial_relations/doctype/contract_of_employment/contract_of_employment.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, BuFf0k and contributors
 # For license information, please see license.txt
 
-import frappe
+import frappe, re
 from frappe.model.document import Document
-from frappe.utils import add_months, add_years
+from frappe.utils import add_months, add_years, format_time
 
 class ContractofEmployment(Document):
     def validate(self):
@@ -15,15 +15,20 @@ class ContractofEmployment(Document):
         """Fetches and updates the contract clauses from the linked Contract Type."""
         if self.contract_type:
             contract_type_doc = frappe.get_doc('Contract Type', self.contract_type)
-            self.contract_clauses = []  # Clear existing clauses
+            self.contract_clauses = []
 
             for term in contract_type_doc.contract_terms:
                 section = frappe.get_doc('Contract Section', term.section)
-                clause_content = f"<b>{section.sec_head}</b><br>"
+                
+                # Numbered section header
+                clause_content = f"<b>{term.sec_no}. {section.sec_head}</b><br>"
                 
                 # Handle section numbering
-                numbered_content = self.handle_section_numbering(section)
+                numbered_content = self.handle_section_numbering(section, term.sec_no)
                 clause_content += numbered_content
+
+                # Add a blank line between sections
+                clause_content += "<br><br>"
 
                 # Append the content to the contract_clauses table
                 self.append('contract_clauses', {
@@ -36,12 +41,18 @@ class ContractofEmployment(Document):
             self.has_retirement = contract_type_doc.has_retirement
             self.retirement_age = contract_type_doc.retirement_age
 
-    def handle_section_numbering(self, section):
+    def handle_section_numbering(self, section, section_number):
         """Handles the numbering and formatting of sections and paragraphs."""
         content = ""
         for par in section.sec_par:
-            par_num = f"{section.sec_head}.{par.par_num}"  # Example of section numbering
-            content += f"{par_num}. {par.par_text}<br>"
+            # Construct the expected placeholder format
+            placeholder_pattern = r'\{{par\."{}"\.(\d+)\}}'.format(re.escape(section.sec_head))
+
+            # Replace placeholders with the actual section number + paragraph number
+            par_num = f"{section_number}.{par.par_num}"
+            formatted_text = re.sub(placeholder_pattern, f'{section_number}.\\1', par.par_text)
+
+            content += f"{par_num}. {formatted_text}<br>"
         return content
 
     def generate_contract(self):
@@ -64,20 +75,22 @@ class ContractofEmployment(Document):
             content = content.replace("{custom_id_number}", self.custom_id_number or "_____________________")
             content = content.replace("{branch}", self.branch or "_____________________")
             content = content.replace("{contract_type}", self.contract_type or "_____________________")
-            content = content.replace("{mon_start}", self.mon_start or "__________")
-            content = content.replace("{mon_end}", self.mon_start or "__________")
-            content = content.replace("{tue_start}", self.tue_start or "__________")
-            content = content.replace("{tue_end}", self.tue_start or "__________")
-            content = content.replace("{wed_start}", self.tue_start or "__________")
-            content = content.replace("{wed_end}", self.tue_start or "__________")
-            content = content.replace("{thu_start}", self.tue_start or "__________")
-            content = content.replace("{thu_end}", self.tue_start or "__________")
-            content = content.replace("{fri_start}", self.tue_start or "__________")
-            content = content.replace("{fri_end}", self.tue_start or "__________")
-            content = content.replace("{sat_start}", self.tue_start or "__________")
-            content = content.replace("{sat_end}", self.tue_start or "__________")
-            content = content.replace("{sun_start}", self.tue_start or "__________")
-            content = content.replace("{sun_end}", self.tue_start or "__________")
+            
+            # Replace time placeholders with 24-hour format
+            content = content.replace("{mon_start}", format_time(self.mon_start or "00:00", "HH:mm"))
+            content = content.replace("{mon_end}", format_time(self.mon_end or "00:00", "HH:mm"))
+            content = content.replace("{tue_start}", format_time(self.tue_start or "00:00", "HH:mm"))
+            content = content.replace("{tue_end}", format_time(self.tue_end or "00:00", "HH:mm"))
+            content = content.replace("{wed_start}", format_time(self.wed_start or "00:00", "HH:mm"))
+            content = content.replace("{wed_end}", format_time(self.wed_end or "00:00", "HH:mm"))
+            content = content.replace("{thu_start}", format_time(self.thu_start or "00:00", "HH:mm"))
+            content = content.replace("{thu_end}", format_time(self.thu_end or "00:00", "HH:mm"))
+            content = content.replace("{fri_start}", format_time(self.fri_start or "00:00", "HH:mm"))
+            content = content.replace("{fri_end}", format_time(self.fri_end or "00:00", "HH:mm"))
+            content = content.replace("{sat_start}", format_time(self.sat_start or "00:00", "HH:mm"))
+            content = content.replace("{sat_end}", format_time(self.sat_end or "00:00", "HH:mm"))
+            content = content.replace("{sun_start}", format_time(self.sun_start or "00:00", "HH:mm"))
+            content = content.replace("{sun_end}", format_time(self.sun_end or "00:00", "HH:mm"))
 
             contract_content += content
 
