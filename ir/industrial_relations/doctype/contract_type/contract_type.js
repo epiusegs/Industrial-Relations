c768f32b56441afbca95bd4391b5665314583004
buff0k <buff0k@gmail.com>
2024-09-17 16:05:43 +0000

    modified:   ir/industrial_relations/doctype/contract_type/contract_type.js 	modified:   ir/industrial_relations/doctype/contract_type/contract_type.json 	modified:   ir/industrial_relations/doctype/contract_type/contract_type.py Added logic to autmoatically add required Working Hours and Remuneration Placeholders to new Contract Type documents

--- ir/industrial_relations/doctype/contract_type/contract_type.js
+++ ir/industrial_relations/doctype/contract_type/contract_type.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contract Type", {
-// 	refresh(frm) {
+frappe.ui.form.on("Contract Type", {
+    onload: function(frm) {
+        // Call the function to populate the child table on form load
+        if (frm.is_new() && frm.doc.contract_terms.length <= 1) {
+            mandatory_contract_terms(frm);
+        }
+    }
+});
 
-// 	},
-// });
+// Function to populate the child table
+function mandatory_contract_terms(frm) {
+    // Add the first row: Remuneration Placeholder
+    let row1 = frm.add_child('contract_terms');
+    row1.sec_no = 4;
+    row1.section = 'Remuneration Placeholder';
+
+    // Add the second row: Working Hours Placeholder
+    let row2 = frm.add_child('contract_terms');
+    row2.sec_no = 6;
+    row2.section = 'Working Hours Placeholder';
+
+    // Refresh the child table to display the new rows
+    frm.refresh_field('contract_terms');
+}
--- ir/industrial_relations/doctype/contract_type/contract_type.py
+++ ir/industrial_relations/doctype/contract_type/contract_type.py
@@ -4,6 +4,5 @@
 #import frappe
 from frappe.model.document import Document
 
-
 class ContractType(Document):
-	pass
+    pass
