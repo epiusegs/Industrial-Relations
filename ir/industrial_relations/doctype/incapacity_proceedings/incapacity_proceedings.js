198de42556d45ce7b389a3eece702e05aa13d3cd
buff0k <buff0k@gmail.com>
2024-11-14 07:44:55 +0000

    modified:   ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.js 	modified:   ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.json 	modified:   ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.py Initial Testing of Incap DocType

--- ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.js
+++ ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.js
@@ -1,8 +1,316 @@
 // Copyright (c) 2024, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Incapacity Proceedings", {
-// 	refresh(frm) {
+frappe.ui.form.on("Incapacity Proceedings", {
+    employee: function(frm) {
+        if (frm.doc.accused) {
+            fetch_employee_data(frm, frm.doc.employee, {
+                'employee_name': 'employee_name',
+                'employee': 'employee_coy',
+                'designation': 'designation',
+                'company': 'company',
+                'date_of_joining': 'engagement_date',
+                'branch': 'branch'
+            }, function() {
+                fetch_default_letter_head(frm, frm.doc.company);
+            });
+            fetch_incapacity_history(frm, frm.doc.employee);
 
-// 	},
-// });
+            frappe.call({
+                method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.check_if_ss',
+                args: {
+                    employee: frm.doc.employee
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('is_ss', r.message.is_ss);
+                        frm.set_value('ss_union', r.message.ss_union);
+                    }
+                }
+            });
+        }
+    },
+
+    refresh: function(frm) {
+        frm.toggle_display(['make_warning_form', 'make_nta_hearing', 'write_disciplinary_outcome_report'], frm.doc.docstatus === 0 && !frm.doc.__islocal && frm.doc.workflow_state !== 'Submitted');
+
+        if (frappe.user.has_role("IR Manager")) {
+            frm.add_custom_button(__('Actions'), function() {}, 'Actions')
+                .addClass('btn-primary')
+                .attr('id', 'actions_dropdown');
+
+            frm.page.add_inner_button(__('Issue NTA'), function() {
+                make_nta_hearing(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Write Outcome Report'), function() {
+                write_disciplinary_outcome_report(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Issue Not Guilty'), function() {
+                make_not_guilty_form(frm);
+            }, 'Actions');
+                
+            frm.page.add_inner_button(__('Issue Suspension'), function() {
+                make_suspension_form(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Issue Demotion'), function() {
+                make_demotion_form(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Issue Pay Deduction'), function() {
+                make_pay_deduction_form(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Issue Dismissal'), function() {
+                make_dismissal_form(frm);
+            }, 'Actions');
+        
+            frm.page.add_inner_button(__('Issue VSP'), function() {
+                make_vsp(frm);
+            }, 'Actions');
+
+            frm.page.add_inner_button(__('Cancel Disciplinary Action'), function() {
+                cancel_disciplinary(frm);
+            }, 'Actions');
+        }
+
+        if (!frm.is_new()) {
+            fetch_linked_documents(frm);
+            fetch_additional_linked_documents(frm);
+            fetch_outcome_dates(frm);
+        }      
+    },
+
+    complainant: function(frm) {
+        if (frm.doc.complainant) {
+            frappe.call({
+                method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_complainant_data',
+                args: {
+                    complainant: frm.doc.complainant
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('compl_name', r.message.compl_name);
+                        frm.set_value('compl_pos', r.message.compl_pos);
+                    }
+                }
+            });
+        }
+    }
+});
+
+function fetch_employee_data(frm, employee, fields, callback) {
+    frappe.call({
+        method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_employee_data',
+        args: {
+            employee: employee,
+            fields: JSON.stringify(fields)
+        },
+        callback: function(res) {
+            if (res.message) {
+                for (let field in res.message) {
+                    frm.set_value(field, res.message[field]);
+                }
+                if (callback) callback();
+            }
+        }
+    });
+}
+
+function fetch_default_letter_head(frm, company) {
+    if (company) {
+        frappe.call({
+            method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_default_letter_head',
+            args: {
+                company: company
+            },
+            callback: function(res) {
+                if (res.message) {
+                    frm.set_value('letter_head', res.message);
+                } else {
+                    frm.set_value('letter_head', '');
+                }
+            }
+        });
+    }
+}
+
+function fetch_incapacity_history(frm, accused) {
+    frappe.call({
+        method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_incapacity_history',
+        args: {
+            employee: employee,
+            current_doc_name: frm.doc.name
+        },
+        callback: function(res) {
+            if (res.message) {
+                frm.clear_table('previous_incapacity_outcomes');
+                res.message.forEach(function(row) {
+                    let child = frm.add_child('previous_incapacity_outcomes');
+                    child.disc_action = row.disc_action;
+                    child.date = row.date;
+                    child.sanction = row.sanction;
+                    child.charges = row.charges;
+                });
+                frm.refresh_field('previous_incapacity_outcomes');
+            }
+        }
+    });
+}
+
+function fetch_linked_documents(frm) {
+    frappe.call({
+        method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_linked_documents',
+        args: {
+            doc_name: frm.doc.name
+        },
+        callback: function(res) {
+            if (res.message) {
+                for (let field in res.message.linked_documents) {
+                    frm.set_value(field, res.message.linked_documents[field].join(', '));
+                }
+                if (res.message.latest_outcome) {
+                    frm.set_value('outcome', res.message.latest_outcome);
+                    frm.set_value('outcome_date', res.message.latest_outcome_date);
+                }
+            }
+        }
+    });
+}
+
+function fetch_outcome_dates(frm) {
+    // Call the new server function to update outcome_start and outcome_end
+    frappe.call({
+        method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.update_outcome_dates',
+        args: {
+            doc_name: frm.doc.name
+        },
+        callback: function(r) {
+            if (r.message) {
+                frm.set_value('outcome_start', r.message.outcome_start || '');
+                frm.set_value('outcome_end', r.message.outcome_end || ''); // Ensure outcome_end is set to an empty string if not present
+                frm.refresh_fields(); // Refresh fields to show updated outcome dates
+                }
+        }
+     });
+}
+
+function fetch_additional_linked_documents(frm) {
+    frappe.call({
+        method: 'ir.industrial_relations.doctype.incapacity_proceedings.incapacity_proceedings.fetch_additional_linked_documents',
+        args: {
+            doc_name: frm.doc.name
+        },
+        callback: function(r) {
+            if (r.message) {
+                if (r.message.linked_nta && !frm.doc.linked_nta) {
+                    frm.set_value('linked_nta', r.message.linked_nta);
+                }
+                if (r.message.linked_outcome && !frm.doc.linked_outcome) {
+                    frm.set_value('linked_outcome', r.message.linked_outcome);
+                }
+            }
+        }
+    });
+}
+
+function make_nta_hearing(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.nta_hearing.nta_hearing.make_nta_hearing",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating NTA Hearing ...")
+    });
+}
+
+function write_disciplinary_outcome_report(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.disciplinary_outcome_report.disciplinary_outcome_report.write_disciplinary_outcome_report",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Disciplinary Outcome Report ...")
+    });
+}
+
+function make_not_guilty_form(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.not_guilty_form.not_guilty_form.make_not_guilty_form",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Not Guilty Form ...")
+    });
+}
+
+function make_suspension_form(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.suspension_form.suspension_form.make_suspension_form",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Suspension Form ...")
+    });
+}
+
+function make_demotion_form(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.demotion_form.demotion_form.make_demotion_form",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Demotion Form ...")
+    });
+}
+
+function make_pay_deduction_form(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.pay_deduction_form.pay_deduction_form.make_pay_deduction_form",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Pay Deduction Form ...")
+    });
+}
+
+function make_dismissal_form(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.dismissal_form.dismissal_form.make_dismissal_form",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating Dismissal Form ...")
+    });
+}
+
+function make_vsp(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.voluntary_seperation_agreement.voluntary_seperation_agreement.make_vsp",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Creating VSP ...")
+    });
+}
+
+function cancel_disciplinary(frm) {
+    frappe.model.open_mapped_doc({
+        method: "ir.industrial_relations.doctype.hearing_cancellation_form.hearing_cancellation_form.cancel_disciplinary",
+        frm: frm,
+        args: {
+            linked_disciplinary_action: frm.doc.name
+        },
+        freeze_message: __("Generating Cancellation Form ...")
+    });
+}
--- ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.py
+++ ir/industrial_relations/doctype/incapacity_proceedings/incapacity_proceedings.py
@@ -1,9 +1,202 @@
 # Copyright (c) 2024, BuFf0k and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
 from frappe.model.document import Document
+from frappe.utils import getdate, today, add_months
 
 
 class IncapacityProceedings(Document):
 	pass
+
+@frappe.whitelist()
+def update_outcome_dates(doc_name):
+    frappe.flags.ignore_permissions = True
+    # Define the linked documents and relevant fields
+    linked_docs = {
+        'linked_demotion': 'Demotion Form',
+        'linked_pay_deduction': 'Pay Deduction Form',
+        'linked_suspension': 'Suspension Form'
+    }
+
+    relevant_fields = {
+        'linked_demotion': ['from_date', 'to_date', 'Demoted from'],
+        'linked_pay_deduction': ['from_date', 'to_date', 'Pay deduction effective from'],
+        'linked_suspension': ['from_date', 'to_date', 'Suspended from']
+    }
+    
+    doc = frappe.get_doc('Disciplinary Action', doc_name)
+    latest_outcome_date = None
+    latest_doc = None
+    latest_outcome_type = None
+
+    for link_field, doc_type in linked_docs.items():
+        linked_doc_name = doc.get(link_field)
+        if linked_doc_name:
+            linked_doc = frappe.get_doc(doc_type, linked_doc_name)
+            outcome_date = linked_doc.get('outcome_date')
+            if outcome_date and (not latest_outcome_date or getdate(outcome_date) > getdate(latest_outcome_date)):
+                latest_outcome_date = outcome_date
+                latest_doc = linked_doc
+                latest_outcome_type = link_field
+
+    outcome_start = ""
+    outcome_end = ""
+
+    if latest_doc:
+        if latest_outcome_type == 'linked_demotion':
+            outcome_start = f"Demoted from {latest_doc.from_date}"
+            outcome_end = f"until {latest_doc.to_date}" if latest_doc.to_date else ""
+        elif latest_outcome_type == 'linked_pay_deduction':
+            outcome_start = f"Pay deduction effective from {latest_doc.from_date}"
+            outcome_end = f"until {latest_doc.to_date}" if latest_doc.to_date else ""
+        elif latest_outcome_type == 'linked_suspension':
+            outcome_start = f"Suspended from {latest_doc.from_date}"
+            outcome_end = f"until {latest_doc.to_date}" if latest_doc.to_date else ""
+    
+    return {
+        'outcome_start': outcome_start,
+        'outcome_end': outcome_end
+    }
+
+
+@frappe.whitelist()
+def fetch_employee_data(employee, fields):
+    frappe.flags.ignore_permissions = True
+
+    # Parse the fields argument from JSON string to dictionary
+    fields = json.loads(fields)
+
+    data = {}
+    for field in fields:
+        value = frappe.db.get_value('Employee', employee, field)
+        data[fields[field]] = value if value else ''
+    
+    return data
+
+@frappe.whitelist()
+def fetch_default_letter_head(company):
+    frappe.flags.ignore_permissions = True
+
+    letter_head = frappe.db.get_value('Company', company, 'default_letter_head')
+    return letter_head if letter_head else ''
+
+@frappe.whitelist()
+def fetch_incapacity_history(accused, current_doc_name):
+    frappe.flags.ignore_permissions = True
+
+    incapacity_actions = frappe.get_all('Incapacity Proceedings', filters={
+        'employee': employee,
+        'name': ['!=', current_doc_name]
+    }, fields=['name', 'outcome_date', 'outcome'])
+
+    history = []
+
+    for action in incapacity_actions:
+        action_doc = frappe.get_doc('Incapacity Proceedings', action.name)
+        charges = action_doc.details_of_incapacity
+        sanction = action_doc.outcome if action_doc.outcome else f"Pending {action_doc.name}"
+
+        history.append({
+            'disc_action': action_doc.name,
+            'date': action_doc.outcome_date,
+            'sanction': sanction,
+            'charges': charges
+        })
+
+    return history
+
+@frappe.whitelist()
+def fetch_linked_documents(doc_name):
+    frappe.flags.ignore_permissions = True
+
+    linked_docs = {
+        "Dismissal Form": "linked_dismissal",
+        "Demotion Form": "linked_demotion",
+        "Pay Deduction Form": "linked_pay_deduction",
+        "Not Guilty Form": "linked_not_guilty",
+        "Suspension Form": "linked_suspension",
+        "Voluntary Seperation Agreement": "linked_vsp",
+        "Hearing Cancellation Form": "linked_cancellation"
+    }
+
+    relevant_fields = {
+        "linked_dismissal": "dismissal_type",
+        "linked_demotion": "demotion_type",
+        "linked_pay_deduction": "pay_deduction_type",
+        "linked_not_guilty": "type_of_not_guilty",
+        "linked_suspension": "suspension_type",
+        "linked_vsp": "vsp_type",
+        "linked_cancellation": "cancellation_type"
+    }
+
+    result = {
+        'linked_documents': {},
+        'latest_outcome': None,
+        'latest_outcome_date': None
+    }
+
+    for doctype, fieldname in linked_docs.items():
+        docs = frappe.get_all(doctype, filters={'linked_disciplinary_action': doc_name}, fields=['name', 'outcome_date'])
+
+        if docs:
+            result['linked_documents'][fieldname] = [doc['name'] for doc in docs]
+            for doc in docs:
+                doc_outcome = frappe.db.get_value(doctype, doc['name'], relevant_fields[fieldname])
+                if doc_outcome:
+                    doc_outcome_date = doc['outcome_date']
+                    if not result['latest_outcome_date'] or doc_outcome_date > result['latest_outcome_date']:
+                        result['latest_outcome_date'] = doc_outcome_date
+                        result['latest_outcome'] = doc_outcome
+    
+    return result
+
+@frappe.whitelist()
+def fetch_additional_linked_documents(doc_name):
+    frappe.flags.ignore_permissions = True
+
+    result = {
+        'linked_nta': None,
+        'linked_outcome': None
+    }
+
+    nta_hearing = frappe.get_all('NTA Hearing', filters={'linked_disciplinary_action': doc_name}, fields=['name'])
+    disciplinary_outcome_report = frappe.get_all('Disciplinary Outcome Report', filters={'linked_disciplinary_action': doc_name}, fields=['name'])
+
+    if nta_hearing:
+        result['linked_nta'] = nta_hearing[0]['name']
+    if disciplinary_outcome_report:
+        result['linked_outcome'] = disciplinary_outcome_report[0]['name']
+    
+    return result
+
+@frappe.whitelist()
+def fetch_complainant_data(complainant):
+    frappe.flags.ignore_permissions = True
+
+    data = {
+        'compl_name': frappe.db.get_value('Employee', complainant, 'employee_name') or '',
+        'compl_pos': frappe.db.get_value('Employee', complainant, 'designation') or ''
+    }
+    
+    return data
+
+@frappe.whitelist()
+def check_if_ss(accused):
+    frappe.flags.ignore_permissions = True
+
+    trade_unions = frappe.get_all('Trade Union', fields=['name'])
+
+    for tu in trade_unions:
+        ss_list = frappe.get_all('Union Shop Stewards', filters={'parent': tu.name, 'parentfield': 'ss_list', 'ss_id': accused}, fields=['ss_id'])
+        if ss_list:
+            return {
+                'is_ss': True,
+                'ss_union': tu.name
+            }
+    return {
+        'is_ss': False,
+        'ss_union': None
+    }
+
